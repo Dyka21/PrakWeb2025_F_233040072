d5ef65d68f1fb88399bd7fa2654c12d96b33cedb
Dyka21 <andikakhaerulana21@gmail.com>
2025-11-03 15:16:10 +0700

    yahut

---
+++ Pertemuan2/OOP/Komik.php
@@ -0,0 +1,24 @@
+<?php
+
+class Komik extends Produk{
+
+    public $jmlHalaman;
+
+public function __construct($judul, $penulis, $penerbit, $harga, $jmlHalaman ) 
+    {
+       parent::__construct($judul, $penulis, $penerbit, $harga);
+       $this->jmlHalaman = $jmlHalaman; 
+    }
+
+
+
+    public function getInfoProduk() {
+
+
+        $infoParent = parent::getInfoProduk();
+
+        return "komik : {$infoParent} - {$this->jmlHalaman} halaman.";
+    }
+}
+
+?>
